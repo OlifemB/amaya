d46eabca97e97ea2c28be223dafef5fb73a502ec
Olifem <olifemb@gmail.com>
2024-10-03 04:40:22 +0300

    absolute

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -19,15 +19,18 @@ export function Main() {
         <main className={'flex flex-col flex-1 futuraRoundDemi '}>
             <section
                 style={{ backgroundImage: `url(${background})` }}
-                className={'flex flex-1 flex-col w-full h-full relative bg-cover bg-no-repeat'}
+                className={' flex flex-1 flex-col w-full h-full relative bg-cover bg-no-repeat'}
             >
                 <Slides />
             </section>
 
             <section className={'relative flex flex-col flex-1 items-center justify-between bg-white px-[50px] gap-4'}>
-                <Cloud className={'w-full h-auto absolute mt-[-10%] z-[1]'} />
+                <Cloud className={'w-full h-auto absolute mt-1 translate-y-[-100%] z-[1] max-w-[540px]'} />
 
-                <div id={'description_header'} className={'flex flex-col gap-2 items-center justify-center'}>
+                <div
+                    id={'description_header'}
+                    className={'flex flex-col gap-2 items-center justify-center max-w-[540px]'}
+                >
                     <img src={LogAmaya} alt={''} className={'w-[160px] h-auto z-[100]'} />
 
                     <span className={'text-center text-primary text-[16px] max-w-48'}>{t('header.title')}</span>
